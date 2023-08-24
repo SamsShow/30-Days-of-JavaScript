ca3f31eb9c9e37386647ee5ef066b6084653e23c
Saksham Tyagi <sakshamtyagi2008@gmail.com>
2023-08-24 15:04:27 +0530

    Day 4

---
+++ counterII.js
@@ -0,0 +1,65 @@
+//Method 1
+
+/*
+var createCounter = function(init) {
+    let presentCount = init;
+  
+    function increment() {
+      return ++presentCount;
+    }
+  
+    function decrement() {
+        return --presentCount;
+    }
+  
+    function reset() {
+        return (presentCount = init);
+    }
+  
+    return { increment, decrement, reset };
+};
+*/
+
+//Method 2
+
+
+var createCounter = function(init) {
+    let presentCount = init
+    return {
+        increment:()=> ++presentCount,
+        decrement:()=> --presentCount,
+        reset:()=> presentCount = init,
+    }
+};
+
+
+//Method 3
+
+/*
+class Counter {
+    constructor(init) {
+      this.init = init;
+      this.presentCount = init;
+    }
+  
+    increment() {
+      this.presentCount += 1;
+      return this.presentCount;
+    }
+  
+    decrement() {
+      this.presentCount -= 1;
+      return this.presentCount;
+    }
+  
+    reset() {
+      this.presentCount = this.init;
+      return this.presentCount;
+    }
+  }
+  
+  var createCounter = function(init) {
+    return new Counter(init);
+};
+
+*/
